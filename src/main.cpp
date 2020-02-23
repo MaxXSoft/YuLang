cd7383d848786bfc00e58722c2284659f24c81f4
MaxXSoft <x@MaxXSoft.net>
2020-02-23 13:31:52 +0800

    (src) added some source files

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "version.h"
+#include "xstl/argparse.h"
+
+using namespace std;
+
+namespace {
+
+//
+
+}  // namespace
+
+int main(int argc, const char *argv[]) {
+  //
+  return 0;
+}
---
+++ src/version.h
@@ -0,0 +1,26 @@
+#ifndef YULANG_VERSION_H_
+#define YULANG_VERSION_H_
+
+// version information of YuLang Compiler
+
+#ifndef APP_NAME
+#define APP_NAME            "YuLang Compiler"
+#endif
+
+#ifndef APP_VERSION
+#define APP_VERSION         "0.0.0"
+#endif
+
+#ifndef APP_VERSION_MAJOR
+#define APP_VERSION_MAJOR   0
+#endif
+
+#ifndef APP_VERSION_MINOR
+#define APP_VERSION_MINOR   0
+#endif
+
+#ifndef APP_VERSION_PATCH
+#define APP_VERSION_PATCH   0
+#endif
+
+#endif  // YULANG_VERSION_H_
