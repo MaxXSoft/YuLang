e84017a554c25f33441930b7d3ca0ce83c81f058
MaxXSoft <x@MaxXSoft.net>
2020-02-25 17:14:51 +0800

    (logger) added column number & error number

--- src/front/logger.cpp
+++ src/front/logger.cpp
@@ -6,12 +6,18 @@
 
 using namespace yulang::front;
 
+// definition of static member variables in logger
+std::size_t Logger::error_num_;
+
 // TODO: show content of file
 
 void Logger::LogError(std::string_view message) const {
   using namespace xstl;
   // print error message
-  std::cerr << style("B") << cur_file_ << ":" << line_pos_ << ": ";
-  std::cerr << style("RBr") << "error: ";
+  std::cerr << style("B") << cur_file_ << ":";
+  std::cerr << style("B") << line_pos_ << ":" << col_pos_ << ":";
+  std::cerr << style("Br") << "error: ";
   std::cerr << message << std::endl;
+  // increase error number
+  ++error_num_;
 }
--- src/front/logger.h
+++ src/front/logger.h
@@ -8,27 +8,37 @@ namespace yulang::front {
 
 class Logger {
  public:
-  Logger() : cur_file_(""), line_pos_(1) {}
-  Logger(std::string_view cur_file) : cur_file_(cur_file), line_pos_(1) {}
+  Logger() : cur_file_(""), line_pos_(1), col_pos_(0) {}
+  Logger(std::string_view cur_file)
+      : cur_file_(cur_file), line_pos_(1), col_pos_(0) {}
 
   // print error message to stderr
   void LogError(std::string_view message) const;
-  // increase line position
-  void IncreaseLinePos() { ++line_pos_; }
 
-  // setters
-  void set_cur_file(std::string_view cur_file) {
-    cur_file_ = cur_file;
+  // reset error number
+  void Reset() {
+    line_pos_ = 1;
+    col_pos_ = 0;
+    error_num_ = 0;
   }
-  void set_line_pos(std::size_t line_pos) { line_pos_ = line_pos; }
+  // increase line position
+  void IncreaseLinePos() {
+    ++line_pos_;
+    col_pos_ = 0;
+  }
+  // increase column position
+  void IncreaseColPos() { ++col_pos_; }
 
   // getters
   std::string_view cur_file() const { return cur_file_; }
   std::size_t line_pos() const { return line_pos_; }
+  std::size_t col_pos() const { return col_pos_; }
+  std::size_t error_num() const { return error_num_; }
 
  private:
+  static std::size_t error_num_;
   std::string_view cur_file_;
-  std::size_t line_pos_;
+  std::size_t line_pos_, col_pos_;
 };
 
 }  // namespace yulang::front
