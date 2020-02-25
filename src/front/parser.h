38e1a9e3cd822045a92b1cfc1cc54581c20b2166
MaxXSoft <x@MaxXSoft.net>
2020-02-25 22:58:57 +0800

    (parser) added class 'Parser'

---
+++ src/front/parser.h
@@ -0,0 +1,170 @@
+#ifndef YULANG_FRONT_PARSER_H_
+#define YULANG_FRONT_PARSER_H_
+
+#include <string_view>
+
+#include "define/token.h"
+#include "define/ast.h"
+#include "front/lexman.h"
+
+namespace yulang::front {
+
+class Parser {
+ public:
+  Parser(LexerManager &lex_man) : lex_man_(lex_man) { Reset(); }
+
+  // reset parser status
+  void Reset() {
+    lexer()->Reset();
+    ended_ = false;
+    in_import_ = false;
+    NextToken();
+  }
+
+  // get next AST from token stream
+  define::ASTPtr ParseNext() {
+    if (cur_token_ == define::Token::End) {
+      ended_ = true;
+      return nullptr;
+    }
+    else {
+      return ParseLine();
+    }
+  }
+
+  // getters
+  // returns true if parser met EOF
+  bool ended() const { return ended_; }
+
+ private:
+  // get next token from lexer
+  define::Token NextToken() {
+    return cur_token_ = lexer()->logger().error_num()
+                            ? define::Token::Error
+                            : lexer()->NextToken();
+  }
+
+  // check if current token is a character (token type 'Other')
+  bool IsTokenChar(char c) const {
+    using namespace define;
+    return (cur_token_ == Token::Other && lexer()->other_val() == c) ||
+           (cur_token_ == Token::Id && lexer()->id_val().size() == 1 &&
+            lexer()->id_val()[0] == ':');
+  }
+
+  // check if current token is a keyword
+  bool IsTokenKeyword(define::Keyword key) const {
+    using namespace define;
+    return cur_token_ == Token::Keyword && lexer()->key_val() == key;
+  }
+
+  // check if current token is an operator
+  bool IsTokenOperator(define::Operator op) const {
+    using namespace define;
+    return cur_token_ == Token::Operator && lexer()->op_val() == op;
+  }
+
+  // check if current token is an assignment operator
+  bool IsAssign() const {
+    using namespace define;
+    return cur_token_ == Token::Operator &&
+           IsOperatorAssign(lexer()->op_val());
+  }
+
+  // create a new AST
+  template <typename T, typename... Args>
+  define::ASTPtr MakeAST(Args &&... args) {
+    auto ast = std::make_unique<T>(std::forward<Args>(args)...);
+    ast->set_logger(lexer()->logger());
+    return ast;
+  }
+
+  // create a new AST with specific logger
+  template <typename T, typename... Args>
+  define::ASTPtr MakeAST(const Logger &logger, Args &&... args) {
+    auto ast = std::make_unique<T>(std::forward<Args>(args)...);
+    ast->set_logger(logger);
+    return ast;
+  }
+
+  // log error and return null pointer
+  define::ASTPtr LogError(std::string_view message);
+
+  define::ASTPtr ParseLine();
+  define::ASTPtr ParseStatement();
+
+  define::ASTPtr ParseVarDef(define::ASTPtr prop);
+  define::ASTPtr ParseLetDef(define::ASTPtr prop);
+  define::ASTPtr ParseDeclare(define::ASTPtr prop);
+  define::ASTPtr ParseTypeAlias(define::ASTPtr prop);
+  define::ASTPtr ParseStruct(define::ASTPtr prop);
+  define::ASTPtr ParseEnum(define::ASTPtr prop);
+  define::ASTPtr ParseImport();
+
+  define::ASTPtr ParseVarElem();
+  define::ASTPtr ParseLetElem();
+  define::ASTPtr ParseArgList();
+  define::ASTPtr ParseEnumList();
+
+  define::ASTPtr ParseBlock();
+  define::ASTPtr ParseBlockLine();
+  define::ASTPtr ParseBlockStatement();
+
+  define::ASTPtr ParseIfElse();
+  define::ASTPtr ParseWhen();
+  define::ASTPtr ParseWhile();
+  define::ASTPtr ParseForIn();
+  define::ASTPtr ParseAsm();
+  define::ASTPtr ParseControl();
+
+  define::ASTPtr ParseWhenElem();
+
+  define::ASTPtr ParseExpr();
+  define::ASTPtr ParseBinary();
+  define::ASTPtr ParseCast();
+  define::ASTPtr ParseUnary();
+  define::ASTPtr ParseFactor();
+
+  define::ASTPtr ParseIfExpr();
+  define::ASTPtr ParseWhenExpr();
+  define::ASTPtr ParseIndex();
+  define::ASTPtr ParseFunCall();
+
+  define::ASTPtr ParseValue();
+  define::ASTPtr ParseInt();
+  define::ASTPtr ParseFloat();
+  define::ASTPtr ParseChar();
+  define::ASTPtr ParseId();
+  define::ASTPtr ParseString();
+  define::ASTPtr ParseBool();
+  define::ASTPtr ParseNull();
+  define::ASTPtr ParseFunDef();
+  define::ASTPtr ParseValInit();
+
+  define::ASTPtr ParseType();
+  define::ASTPtr ParseValType();
+  define::ASTPtr ParsePrimType();
+  define::ASTPtr ParsePointer();
+  define::ASTPtr ParseArray();
+  define::ASTPtr ParseRef();
+  define::ASTPtr ParseFunc();
+
+  // try to get property and goto next token
+  define::PropertyAST::Property GetProp();
+  // make sure current token is specific character and goto next token
+  bool CheckChar(char c);
+
+  // private getters
+  // current lexer
+  const LexerPtr &lexer() const { return lex_man_.lexer(); }
+
+  LexerManager &lex_man_;
+  define::Token cur_token_;
+  bool ended_;
+  // flag is true if is parsing an 'import' file
+  bool in_import_;
+};
+
+}  // namespace yulang::front
+
+#endif  // YULANG_FRONT_PARSER_H_
