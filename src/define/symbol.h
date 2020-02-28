d332767d0a8435dcae58ba9d81e5724978d5acb1
MaxXSoft <x@MaxXSoft.net>
2020-02-28 21:29:33 +0800

    (symbol) added more definitions

--- src/define/symbol.h
+++ src/define/symbol.h
@@ -2,6 +2,8 @@
 #define YULANG_DEFINE_SYMBOL_H_
 
 #include <string>
+#include <variant>
+#include <optional>
 
 #include "define/type.h"
 #include "xstl/nested.h"
@@ -11,14 +13,28 @@ namespace yulang::define {
 // scope environment (symbol table)
 using EnvPtr = xstl::NestedMapPtr<std::string, TypePtr>;
 
+// evaluated values
+using EvalNum = std::variant<std::uint32_t, float, double>;
+using EvalEnvPtr = xstl::NestedMapPtr<std::string, std::optional<EvalNum>>;
+
 // make a new environment
-inline EnvPtr MakeEnvironment() {
+inline EnvPtr MakeEnv() {
   return xstl::MakeNestedMap<std::string, TypePtr>();
 }
 
 // make a new environment (with outer environment)
-inline EnvPtr MakeEnvironment(const EnvPtr &outer) {
+inline EnvPtr MakeEnv(const EnvPtr &outer) {
   return xstl::MakeNestedMap<std::string, TypePtr>(outer);
+}
+
+// make a new evaluation environment
+inline EvalEnvPtr MakeEvalEnv() {
+  return xstl::MakeNestedMap<std::string, std::optional<EvalNum>>();
+}
+
+// make a new environment (with outer environment)
+inline EvalEnvPtr MakeEvalEnv(const EvalEnvPtr &outer) {
+  return xstl::MakeNestedMap<std::string, std::optional<EvalNum>>(outer);
 }
 
 }  // namespace yulang::define
