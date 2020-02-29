cd62aa1e842b4ac36cb5cb5a9a6fdc9339ca5d10
MaxXSoft <x@MaxXSoft.net>
2020-02-29 18:51:24 +0800

    (main) added some details

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 #include "version.h"
-#include "xstl/argparse.h"
+#include "front/lexman.h"
+#include "front/parser.h"
+#include "front/analyzer.h"
 
 using namespace std;
+using namespace yulang::front;
 
 namespace {
 
@@ -12,6 +15,14 @@ namespace {
 }  // namespace
 
 int main(int argc, const char *argv[]) {
-  //
-  return 0;
+  if (argc < 2) return 1;
+  LexerManager lex_man;
+  lex_man.LoadSource(argv[1]);
+  Parser parser(lex_man);
+  Analyzer ana;
+  while (auto ast = parser.ParseNext()) {
+    ast->Dump(cout);
+    if (!ast->SemaAnalyze(ana)) break;
+  }
+  return lex_man.lexer()->logger().error_num();
 }
