6f53512ef9eddd16ec913c57bb6d4722108631d5
MaxXSoft <x@MaxXSoft.net>
2020-03-21 08:56:57 +0800

    (main) modified some details

--- src/main.cpp
+++ src/main.cpp
@@ -21,16 +21,20 @@ int main(int argc, const char *argv[]) {
   if (argc < 2) return 1;
   LexerManager lex_man;
   lex_man.LoadSource(argv[1]);
+
   Parser parser(lex_man);
   Evaluator eval;
   Analyzer ana(eval);
   LLVMBuilder builder(argv[1]);
+  yulang::define::SetPointerSize(builder.GetPointerSize());
+
   while (auto ast = parser.ParseNext()) {
     if (!ast->SemaAnalyze(ana)) break;
     ast->Eval(eval);
     // ast->Dump(cout);
     ast->GenerateIR(builder);
   }
+
   auto err_num = lex_man.lexer()->logger().error_num();
   if (!err_num) builder.Dump(cout);
   return lex_man.lexer()->logger().error_num();
