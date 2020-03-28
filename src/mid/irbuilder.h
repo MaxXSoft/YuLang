c2659f4377e9654723bfea7be21296b27a0b90be
MaxXSoft <x@MaxXSoft.net>
2020-03-28 19:44:55 +0800

    (irbuilder) added class 'IRBuilder'

---
+++ src/mid/irbuilder.h
@@ -0,0 +1,16 @@
+#ifndef YULANG_MID_IRBUILDER_H_
+#define YULANG_MID_IRBUILDER_H_
+
+namespace yulang::mid {
+
+class IRBuilder {
+ public:
+  IRBuilder() {}
+
+ private:
+  //
+};
+
+}  // namespace yulang::mid
+
+#endif  // YULANG_MID_IRBUILDER_H_
