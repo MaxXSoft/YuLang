c578981aabc179386a09acbfd919b0b01209666f
MaxXSoft <x@MaxXSoft.net>
2020-04-01 23:04:49 +0800

    (codegen) updated interface definition

--- src/back/codegen.cpp
+++ src/back/codegen.cpp
@@ -1,166 +1,90 @@
-#include "define/ast.h"
+#include "mid/ssa.h"
 
 #include "back/codegen.h"
 
-using namespace yulang::define;
+using namespace yulang::mid;
 using namespace yulang::back;
 
-CodePtr VarLetDefAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void LoadSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr FunDefAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void StoreSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr DeclareAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void AccessSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr TypeAliasAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void BinarySSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr StructAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void UnarySSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr EnumAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void CallSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr ImportAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void BranchSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr VarLetElemAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void JumpSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr ArgElemAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void ReturnSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr StructElemAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void FunctionSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr EnumElemAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void GlobalVarSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr BlockAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void AllocaSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr IfAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void BlockSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr WhenAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void ArgRefSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr WhileAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void AsmSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr ForInAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void ConstIntSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr AsmAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void ConstFloatSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr ControlAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void ConstStrSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr WhenElemAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void ConstStructSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr BinaryAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void ConstArraySSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
 
-CodePtr AccessAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr CastAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr UnaryAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr IndexAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr FunCallAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr IntAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr FloatAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr CharAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr IdAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr StringAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr BoolAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr NullAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr ValInitAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr PrimTypeAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr UserTypeAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr FuncTypeAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr VolaTypeAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr ArrayTypeAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr PointerTypeAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
-}
-
-CodePtr RefTypeAST::GenerateCode(CodeGen &gen) {
-  return gen.GenerateOn(*this);
+void ConstZeroSSA::GenerateCode(CodeGen &gen) {
+  gen.GenerateOn(*this);
 }
--- src/back/codegen.h
+++ src/back/codegen.h
@@ -1,10 +1,10 @@
 #ifndef YULANG_BACK_CODEGEN_H_
 #define YULANG_BACK_CODEGEN_H_
 
+#include <ostream>
 #include <cstddef>
 
-#include "back/code.h"
-#include "define/ast.h"
+#include "mid/ssa.h"
 
 namespace yulang::back {
 
@@ -13,51 +13,32 @@ class CodeGenInterface {
  public:
   virtual ~CodeGenInterface() = default;
 
-  virtual CodePtr GenerateOn(define::VarLetDefAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::FunDefAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::DeclareAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::TypeAliasAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::StructAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::EnumAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::ImportAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::VarLetElemAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::ArgElemAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::StructElemAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::EnumElemAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::BlockAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::IfAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::WhenAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::WhileAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::ForInAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::AsmAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::ControlAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::WhenElemAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::BinaryAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::AccessAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::CastAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::UnaryAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::IndexAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::FunCallAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::IntAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::FloatAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::CharAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::IdAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::StringAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::BoolAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::NullAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::ValInitAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::PrimTypeAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::UserTypeAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::FuncTypeAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::VolaTypeAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::ArrayTypeAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::PointerTypeAST &ast) = 0;
-  virtual CodePtr GenerateOn(define::RefTypeAST &ast) = 0;
+  virtual void GenerateOn(mid::LoadSSA &ssa) = 0;
+  virtual void GenerateOn(mid::StoreSSA &ssa) = 0;
+  virtual void GenerateOn(mid::AccessSSA &ssa) = 0;
+  virtual void GenerateOn(mid::BinarySSA &ssa) = 0;
+  virtual void GenerateOn(mid::UnarySSA &ssa) = 0;
+  virtual void GenerateOn(mid::CallSSA &ssa) = 0;
+  virtual void GenerateOn(mid::BranchSSA &ssa) = 0;
+  virtual void GenerateOn(mid::JumpSSA &ssa) = 0;
+  virtual void GenerateOn(mid::ReturnSSA &ssa) = 0;
+  virtual void GenerateOn(mid::FunctionSSA &ssa) = 0;
+  virtual void GenerateOn(mid::GlobalVarSSA &ssa) = 0;
+  virtual void GenerateOn(mid::AllocaSSA &ssa) = 0;
+  virtual void GenerateOn(mid::BlockSSA &ssa) = 0;
+  virtual void GenerateOn(mid::ArgRefSSA &ssa) = 0;
+  virtual void GenerateOn(mid::AsmSSA &ssa) = 0;
+  virtual void GenerateOn(mid::ConstIntSSA &ssa) = 0;
+  virtual void GenerateOn(mid::ConstFloatSSA &ssa) = 0;
+  virtual void GenerateOn(mid::ConstStrSSA &ssa) = 0;
+  virtual void GenerateOn(mid::ConstStructSSA &ssa) = 0;
+  virtual void GenerateOn(mid::ConstArraySSA &ssa) = 0;
+  virtual void GenerateOn(mid::ConstZeroSSA &ssa) = 0;
 
   // get the size of pointer
   virtual std::size_t GetPointerSize() const = 0;
-  // dump IRs in current builder
-  virtual void Dump(std::ostream &os) = 0;
+  // dump code in current generator
+  virtual void Dump(std::ostream &os) const = 0;
 };
 
 }  // namespace yulang::back
