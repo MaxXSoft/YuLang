19b59b668db3b3aa77506268f2ad6ee90527d283
MaxXSoft <x@MaxXSoft.net>
2020-04-01 19:50:54 +0800

    (main) added more details

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 #include "version.h"
 
 #include "define/type.h"
+#include "front/logger.h"
 #include "front/lexman.h"
 #include "front/parser.h"
 #include "front/analyzer.h"
 #include "front/eval.h"
 #include "mid/irbuilder.h"
+#include "mid/passman.h"
 
 #include "xstl/argparse.h"
 
@@ -28,6 +31,50 @@ void PrintVersion() {
   cout << endl;
 }
 
+void CompileToIR(const xstl::ArgParser &argp, std::ostream &os,
+                 IRBuilder &irb) {
+  // initialize lexer manager & logger
+  auto file = argp.GetValue<string>("input");
+  auto imp_path = argp.GetValue<vector<string>>("imppath");
+  LexerManager lex_man;
+  lex_man.LoadSource(file);
+  if (!imp_path.empty()) {
+    for (const auto &i : imp_path) lex_man.AddImportPath(1, i);
+  }
+  Logger::ResetErrorNum(argp.GetValue<bool>("warn-error"));
+  // initialize other stuffs
+  Parser parser(lex_man);
+  Evaluator eval;
+  Analyzer ana(eval);
+  // get output info
+  auto out_type = argp.GetValue<string>("outtype");
+  auto dump_ast = out_type == "ast";
+  // compile source code
+  while (auto ast = parser.ParseNext()) {
+    // perform semantic analyze
+    if (!ast->SemaAnalyze(ana)) break;
+    ast->Eval(eval);
+    // dump to output
+    if (dump_ast) ast->Dump(os);
+    // generate IR
+    ast->GenerateIR(irb);
+  }
+}
+
+bool RunPasses(const xstl::ArgParser &argp, IRBuilder &irb) {
+  // run passes on IR
+  PassManager pass_man;
+  auto opt_level = argp.GetValue<int>("opt-level");
+  if (opt_level < 0 || opt_level > 3) {
+    Logger::LogRawError("invalid optimization level");
+    return false;
+  }
+  pass_man.set_opt_level(opt_level);
+  if (argp.GetValue<bool>("verbose")) pass_man.ShowInfo(cerr);
+  irb.module().RunPasses(pass_man);
+  return true;
+}
+
 }  // namespace
 
 int main(int argc, const char *argv[]) {
@@ -40,8 +87,12 @@ int main(int argc, const char *argv[]) {
                          "type of output (ast/yuir/llvm/obj)", "yuir");
   argp.AddOption<string>("output", "o", "output file, default to stdout",
                          "");
-  argp.AddOption<string>("imppath", "I", "set directory of import path",
-                         "");
+  argp.AddOption<vector<string>>("imppath", "I", "add directory to "
+                                 "import search path", {});
+  argp.AddOption<int>("opt-level", "O", "set optimization level (0-3)", 0);
+  argp.AddOption<bool>("verbose", "V", "use verbose output", false);
+  argp.AddOption<bool>("warn-error", "Werror", "treat warnings as errors",
+                       false);
 
   // parse argument
   auto ret = argp.Parse(argc, argv);
@@ -61,23 +112,9 @@ int main(int argc, const char *argv[]) {
     return 1;
   }
 
-  // initialize lexer manager
-  auto file = argp.GetValue<string>("input");
-  auto imp_path = argp.GetValue<string>("imppath");
-  LexerManager lex_man;
-  lex_man.LoadSource(file);
-  if (!imp_path.empty()) lex_man.AddImportPath(1, imp_path);
-
-  // initialize other stuffs
-  Parser parser(lex_man);
-  Evaluator eval;
-  Analyzer ana(eval);
-  IRBuilder irb;
-
   // get output info
   auto out_type = argp.GetValue<string>("outtype");
   auto out_file = argp.GetValue<string>("output");
-  auto dump_ast = out_type == "ast";
   auto dump_yuir = out_type == "yuir";
 
   // initialize output stream
@@ -85,19 +122,20 @@ int main(int argc, const char *argv[]) {
   if (!out_file.empty()) ofs.open(out_file);
   auto &os = out_file.empty() ? cout : ofs;
 
-  // compile source code
-  while (auto ast = parser.ParseNext()) {
-    // perform semantic analyze
-    if (!ast->SemaAnalyze(ana)) break;
-    ast->Eval(eval);
-    // dump to output
-    if (dump_ast) ast->Dump(os);
-    // generate IR
-    ast->GenerateIR(irb);
-  }
+  // compile source to IR
+  IRBuilder irb;
+  CompileToIR(argp, os, irb);
 
-  // check if is error
-  auto err_num = lex_man.lexer()->logger().error_num();
-  if (!err_num && dump_yuir) irb.module().Dump(os);
-  return err_num;
+  // check if is error after IR generation
+  if (auto err_num = Logger::error_num()) return err_num;
+
+  // run passes on IR
+  RunPasses(argp, irb);
+
+  // check if is error after passes running
+  if (auto err_num = Logger::error_num()) return err_num;
+
+  // dump IR
+  if (dump_yuir) irb.module().Dump(os);
+  return 0;
 }
