d322aa32ccb3e99b7da7d5246c42a291cfee6a9d
MaxXSoft <x@MaxXSoft.net>
2021-03-10 17:11:39 +0800

    (analyzer) fixed faults about handling reference types

--- src/front/analyzer.cpp
+++ src/front/analyzer.cpp
@@ -359,9 +359,14 @@ TypePtr Analyzer::AnalyzeOn(VarLetElemAST &ast) {
     assert(!type->IsRightValue());
     if (init && !CheckInit(log, type, init, id)) return nullptr;
     // check for reference types
-    if (!init && type->IsReference()) {
-      return LogError(log, "cannot define a reference "
-                      "without initialization", id);
+    if (type->IsReference()) {
+      if (!init) {
+        return LogError(log, "cannot define a reference "
+                        "without initialization", id);
+      }
+      if (ast.is_var()) {
+        log.LogWarning("references should only be defined with 'let'");
+      }
     }
     sym_type = std::move(type);
   }
@@ -377,11 +382,13 @@ TypePtr Analyzer::AnalyzeOn(VarLetElemAST &ast) {
     sym_type = std::move(init);
   }
   // add symbol info
-  if (ast.is_var()) {
-    if (sym_type->IsConst()) sym_type = sym_type->GetDeconstedType();
-  }
-  else if (!sym_type->IsConst()) {
-    sym_type = std::make_shared<ConstType>(std::move(sym_type));
+  if (!sym_type->IsReference()) {
+    if (ast.is_var()) {
+      if (sym_type->IsConst()) sym_type = sym_type->GetDeconstedType();
+    }
+    else if (!sym_type->IsConst()) {
+      sym_type = std::make_shared<ConstType>(std::move(sym_type));
+    }
   }
   symbols_->AddItem(id, sym_type);
   ast.set_ast_type(std::move(sym_type));
